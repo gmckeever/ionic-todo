e4c74069180d42e62ee98eb8b8f52592dc922cb3
Greg McKeever <gmckeever@gmail.com>
2013-12-14 01:03:39 +0000

    added controller

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,30 @@
-angular.module('todo', ['ionic']);
+angular.module('todo', ['ionic'])
 
+.controller('TodoCtrl', function($scope, Modal) {
+  // No need for testing data anymore
+  $scope.tasks = [];
 
+  // Create and load the Modal
+  Modal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push(task);
+    $scope.taskModal.hide();
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+});
