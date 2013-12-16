f041f3bc8f76e4ac357dfa7ad92e94f804c8c006
Greg McKeever <gmckeever@gmail.com>
2013-12-16 09:12:40 +0000

    Integrated Firebase with AngularFire

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('todo', ['ionic'])
+angular.module('todo', ['ionic','firebase'])
 /**
  * The Projects factory handles saving and loading projects
  * from local storage, and also lets us save and load the
@@ -32,87 +32,75 @@ angular.module('todo', ['ionic'])
   }
 })
 
-.controller('TodoCtrl', function($scope, $timeout, Modal, Projects) {
+    .controller('TodoCtrl', function($scope, $firebase, $timeout, Modal, Projects) {
 
-  // A utility function for creating a new project
-  // with the given projectTitle
-  var createProject = function(projectTitle) {
-    var newProject = Projects.newProject(projectTitle);
-    $scope.projects.push(newProject);
-    Projects.save($scope.projects);
-    $scope.selectProject(newProject, $scope.projects.length-1);
-  }
+        $scope.projectsList = {};
+        $scope.user = {};
+        // Load or initialize projects
+        $scope.projectsList = $firebase(new Firebase("https://nitrous-io.firebaseio.com/projects"));
+        $scope.user = $firebase(new Firebase("https://nitrous-io.firebaseio.com/Users/Greg"));
 
+        $scope.projectsList.$on("loaded", function() {
+            console.log($scope.projectsList);
+            window.projectsList = $scope.projectsList;
+        });
 
-  // Load or initialize projects
-  $scope.projects = Projects.all();
-
-  // Grab the last active, or the first project
-  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
-
-  // Called to create a new project
-  $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
-    if(projectTitle) {
-      createProject(projectTitle);
-    }
-  };
-
-  // Called to select the given project
-  $scope.selectProject = function(project, index) {
-    $scope.activeProject = project;
-    Projects.setLastActiveIndex(index);
-    $scope.sideMenuController.close();
-  };
-
-  // Create our modal
-  Modal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope
-  });
-
-  $scope.createTask = function(task) {
-    if(!$scope.activeProject) {
-      return;
-    }
-    $scope.activeProject.tasks.push({
-      title: task.title
-    });
-    $scope.taskModal.hide();
-
-    // Inefficient, but save all the projects
-    Projects.save($scope.projects);
-
-    task.title = "";
-  };
-
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  }
-
-  $scope.toggleProjects = function() {
-    $scope.sideMenuController.toggleLeft();
-  };
-
-
-  // Try to create the first project, make sure to defer
-  // this by using $timeout so everything is initialized
-  // properly
-  $timeout(function() {
-    if($scope.projects.length == 0) {
-      while(true) {
-        var projectTitle = prompt('Your first project title:');
-        if(projectTitle) {
-          createProject(projectTitle);
-          break;
+        $scope.projectsList.$on("loaded", function() {
+            console.log($scope.user);
+            window.user = $scope.user;
+        });
+        // A utility function for creating a new project
+        // with the given projectTitle
+        var createProject = function(projectTitle) {
+            $scope.projectsList[projectTitle] = [];
+            $scope.projectsList.$save(projectTitle);
+            $scope.selectProject(projectTitle);
         }
-      }
-    }
-  });
+
+        // Called to create a new project
+        $scope.newProject = function() {
+            var projectTitle = prompt('Project name');
+            if(projectTitle) {
+                createProject(projectTitle);
+            }
+        };
+
+        // Called to select the given project
+        $scope.selectProject = function(project) {
+            $scope.user.lastproject = project;
+            $scope.sideMenuController.close();
+        };
+
+        // Create our modal
+        Modal.fromTemplateUrl('new-task.html', function(modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope
+        });
+
+        $scope.createTask = function(task) {
+            if(!$scope.user.lastproject) {
+                return;
+            }
+
+            var name = $scope.user.lastproject;
+            $scope.projectsList[name].push(task.title);
+            $scope.projectsList.$save(name);
+            $scope.taskModal.hide();
+
+            task.title = "";
+        };
+
+        $scope.newTask = function() {
+            $scope.taskModal.show();
+        };
+
+        $scope.closeNewTask = function() {
+            $scope.taskModal.hide();
+        }
+
+        $scope.toggleProjects = function() {
+            $scope.sideMenuController.toggleLeft();
+        };
 
 });
